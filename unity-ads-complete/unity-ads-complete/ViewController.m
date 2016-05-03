8f8db42c2381d8c20396599d8084e41a4de6f95f
Fritz Huie <fritzhuie@gmail.com>
2016-05-03 16:02:51 -0700

    cleaned up example project

--- unity-ads-complete/unity-ads-complete/ViewController.m
+++ unity-ads-complete/unity-ads-complete/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+#import <UnityAds/UnityAds.h>
 
-@interface ViewController ()
+@interface ViewController () <UnityAdsDelegate>
+@property (weak, nonatomic) IBOutlet UIButton *videoButton;
+@property (weak, nonatomic) IBOutlet UIButton *rewardedButton;
+@property (weak, nonatomic) IBOutlet UILabel *loadingLabel;
 
 @end
 
@@ -18,25 +22,35 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    _videoButton.hidden = true;
+    _rewardedButton.hidden = true;
+    
+    [[UnityAds sharedInstance] setDelegate:self];
 }
 
 - (IBAction)showAd:(id)sender {
-    if ([[UnityAds sharedInstance]canShowZone:@"video"]) {
-        [[UnityAds sharedInstance] setZone:@"video"];
-        [[UnityAds sharedInstance] show];
-    }
-}
-- (IBAction)showRewardedAd:(id)sender {
-    if ([[UnityAds sharedInstance]canShowZone:@"rewardedVideo"]) {
-        [[UnityAds sharedInstance] setZone:@"rewardedVideo"];
+    if ([[UnityAds sharedInstance]canShow] && [[UnityAds sharedInstance] setZone:@"video"]) {
         [[UnityAds sharedInstance] show];
     }
 }
 
+- (IBAction)showRewardedAd:(id)sender {
+    if ([[UnityAds sharedInstance]canShow] && [[UnityAds sharedInstance] setZone:@"rewardedVideo"]) {
+        [[UnityAds sharedInstance] show];
+    }
+}
+
+//--- Required callback ---
 - (void)unityAdsVideoCompleted:(NSString *)rewardItemKey skipped:(BOOL)skipped{
+    NSLog(@"Video ended: %@", skipped ? @"skipped" : @"completed");
     if (!skipped) {
         //Add code here to reward player for watching an ad
     }
 }
 
+- (void)unityAdsFetchCompleted {
+    _loadingLabel.hidden = true;
+    _videoButton.hidden = false;
+    _rewardedButton.hidden = false;
+}
 @end
